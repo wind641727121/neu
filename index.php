6cca72b8d087a850c6a2f381a8171bf8c3745bbb
wind grin <641727121@qq.com>
2017-12-12 17:12:36 +0800

    hha

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    $sso ="";
+    $name ="";
+
+    if(isset($_POST["submit"])){
+        $input = $_POST["input"];
+        $dbhost = 'localhost:3306';  //mysql服务器主机地址
+        $dbuser = 'root';      //mysql用户名
+        $dbpass = 'new_password';//mysql用户名密码
+        $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+        mysqli_set_charset($conn,"utf8");
+        if(!$conn ) {
+          die('Could not connect:' . mysql_error());
+        }
+        mysqli_query($conn,"set names 'utf8'");
+        mysqli_select_db($conn,'NEUSTU');
+        
+        $sql = "select* from student where (sso='$input') or (name='$input')";
+        $result = mysqli_query($conn,$sql);  
+        $num = mysqli_num_rows($result);  
+        if($num) {  
+          $row = mysqli_fetch_array($result);  //将数据以索引方式储存在数组中  
+          $name = $row[1];
+          $sso = $row[0];
+        }  
+  }    
+?>
